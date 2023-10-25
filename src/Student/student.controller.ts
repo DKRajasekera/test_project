f2d52e33cf509c40acbe17e98b798124ed8ee11b
Dulaksha Rajasekera <dkrajasekere@gmail.com>
2023-10-25 14:34:28 +0530

    add new functions

--- src/Student/student.controller.ts
+++ src/Student/student.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { AppService } from './student.service';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
+import { StudentService } from './student.service';
 
-@Controller('stubent')
-export class AppController {
-  constructor(private readonly studentService: AppService) {}
+@Controller('student')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
 
   @Post()
   addStudent(
@@ -13,7 +13,42 @@ export class AppController {
     @Body('address') address: String,
     @Body('number') number: number
     ): any {
-      const generateID = this.studentService.AddAStudent(f_name,l_name,dob,address,number);
+      const generateID = this.studentService.AddAStudent(
+        f_name,
+        l_name,
+        dob,
+        address,
+        number
+        );
       return {id: generateID };
-  }
-}
+    }
+
+    @Get()
+    getAllStudents() {
+      return this.studentService.GetStudents();
+    }
+
+    @Get(':id')
+    getStudent(@Param('id') stuID: String) {
+      return this.studentService.FindStudent(stuID);
+    }
+
+    @Patch(':id')
+        @Patch(':id')
+        updateStudent(
+          @Param('id') stuId: string,
+          @Body('first name') f_name: string,
+          @Body('last name') l_name: string,
+          @Body('date of birth') dob: string,
+          @Body('address') address: string,
+          @Body('number') number: number
+        ): void {
+          this.studentService.UpdateStudent(stuId, f_name, l_name, dob, address, number);
+        }
+
+        @Delete(':id')
+        deleteStudent(@Param('id') stuId: string): void {
+          this.studentService.DeleteStudent(stuId);
+          return null;
+        }
+      }
--- src/Student/student.service.ts
+++ src/Student/student.service.ts
@@ -1,14 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { Student } from './student.modul';
 @Injectable()
-export class AppService {
-  student : Student [] = [];
+export class StudentService {
+  private student : Student [] = [];
 
   AddAStudent(F_name: String, L_name: String, dob: String, adress: String, number: number) {
-    const stuID = new Date().toString()
+    const stuID = Math.random().toString()
     const newStudent = new Student(stuID, F_name, L_name, dob, adress, number)
     this.student.push(newStudent)
     return stuID
+  }
+
+  GetStudents() {
+    return [...this.student];
+  }
+
+  FindStudent(studentId) {
+    const student = this.student.find((stu) => stu.id === studentId);
+    if(!student){
+      throw new NotFoundException('Could not found that student');
+    }
+    return {...student}
+  }
+
+  UpdateStudent(studentId, F_name: string, L_name: string, dob: string, address: string, number: number) {
+    const { student, studentIndex } = this.FindStudentIndex(studentId);
+    const updatedStudent = { ...student };
+    if (F_name) {
+      updatedStudent.FirstName = F_name;
+    }
+    if (L_name) {
+      updatedStudent.LastName = L_name;
+    }
+    if (dob) {
+      updatedStudent.DOB = dob;
+    }
+    if (address) {
+      updatedStudent.Address = address;
+    }
+    if (number) {
+      updatedStudent.Number = number;
+    }
+    this.student[studentIndex] = updatedStudent;
+  }
+
+  FindStudentIndex(studentId): { student: Student; studentIndex: number } {
+    const studentIndex = this.student.findIndex((stu) => stu.id === studentId);
+    const student = this.student[studentIndex];
+    if (!student) {
+      throw new NotFoundException('Could not find that student');
+    }
+    return { student, studentIndex };
+  }
+
+  DeleteStudent(studentId) {
+    const index = this.FindStudentIndex(studentId).studentIndex;
+    this.student.splice(index, 1);
   }
 }

93adbb3bfd29e4d05e68b1e414d168e3bcf16a8a
Dulaksha Rajasekera <dkrajasekere@gmail.com>
2023-10-25 11:22:09 +0530

    make some changers

--- src/Student/student.module.ts
+++ src/Student/student.module.ts
@@ -2,8 +2,6 @@ import { Module } from "@nestjs/common";
 import { StudentController } from "./student.controller";
 import { StudentService } from "./student.service";
 
-
-
 @Module ({
     controllers: [StudentController],
     providers: [StudentService]
